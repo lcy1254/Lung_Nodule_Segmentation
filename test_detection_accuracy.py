39df713721e6f7c78d6ebfb231914d7b0144b78f
cylee <lcy12544@gmail.com>
2020-11-30 16:34:04 -0500

    update

--- test_detection_accuracy.py
+++ test_detection_accuracy.py
@@ -43,30 +43,31 @@ for predIter in os.listdir(predDir):
         dice = float(0)
         dicewoFP = float(0)
         img_count = 0
-            for predFile in os.listdir(os.path.join(predDir, predIter, 'prediction')):
-                if '.nii.gz' in predFile:
-                    img_num = (re.findall(r'[0-9]+', predFile))[0]
-                    print(img_num)
+        for predFile in os.listdir(os.path.join(predDir, predIter, 'prediction')):
+            if '.nii.gz' in predFile:
+                img_num = (re.findall(r'[0-9]+', predFile))[0]
+                print(img_num)
 
-                    predimg = nib.load(os.path.join(predDir, predIter, 'prediction', predFile))
-                    gtimg = nib.load(os.path.join(gtDir, 'labels-{}.nii.gz'.format(img_num)))
-                    img_count += 1
+                predimg = nib.load(os.path.join(predDir, predIter, 'prediction', predFile))
+                gtimg = nib.load(os.path.join(gtDir, 'labels-{}.nii.gz'.format(img_num)))
+                img_count += 1
 
-                    s = np.ones((3,3,3))
-                    labeled, nod_num = label(gtimg.dataobj, s)
-                    nods = {}
+                s = np.ones((3,3,3))
+                labeled, nod_num = label(gtimg.dataobj, s)
+                nods = {}
                         
-                    for i in range(nod_num):
-                        nods[i] = da.Nod(labeled, i + 1)
+                for i in range(nod_num):
+                    nods[i] = da.Nod(labeled, i + 1)
 
-                    confusion_matrix += da.Nod.computeConfusion(nods, gtimg.dataobj, predimg.dataobj)
-                    dice += da.Nod.DetectionDice(gtimg.dataobj, predimg.dataobj)
-                    dicewoFP += da.Nod.DetectionDiceWoFP(gtimg.dataobj, predimg.dataobj)
+                confusion_matrix += da.Nod.computeConfusion(nods, gtimg.dataobj, predimg.dataobj)
+                dice += da.Nod.DetectionDice(gtimg.dataobj, predimg.dataobj)
+                dicewoFP += da.Nod.DetectionDiceWoFP(gtimg.dataobj, predimg.dataobj)
         finalDice = dice/img_count
         finalDicewoFP = dicewoFP/img_count
         print(confusion_matrix)
         print(finalDice)
         print(finalDicewoFP)
+        print(img_count)
         
         outDir = os.path.join(predDir, predIter, 'eval')
         if not os.path.isdir(outDir):
