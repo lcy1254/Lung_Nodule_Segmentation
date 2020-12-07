393aa4f6aa39e15478806f8eeef9977adeb3ad7b
cylee <lcy12544@gmail.com>
2020-12-07 03:19:29 -0500

    update assertion error

--- att.py
+++ att.py
@@ -15,10 +15,32 @@ def getAtts(nods, pid):
     
     allNodules = scan.cluster_annotations()
     nodules = [allNodules[i][0] for i in range(len(allNodules))]   #weakness -- only using one annotation when four is provided
-    assert len(nodules) == len(allNodules)
-    assert len(nodules) == len(nods)
+    try:
+        assert len(nodules) == len(allNodules)
+        assert len(nodules) == len(nods)
 
-    for a in range(len(nodules)):
+        for a in range(len(nodules)):
+            nod_mask, nod_bbox, ann_masks = consensus(allNodules[a])
+            scan_vol = np.zeros(shape)
+            scan_vol[nod_bbox] = nod_mask
+            temp_vol = reshape_mask(scan_vol, image)
+            final_vol = temp_vol[x1:x2, y1:y2, z1:z2]
+            for b in range(len(nods)):
+                numIntersect = np.sum(final_vol & nods[b].array)
+                if numIntersect == np.sum(final_vol):
+                    nods[b].subtlety = int(nodules[a].subtlety)
+                    nods[b].internalStructure = int(nodules[a].internalStructure)
+                    nods[b].calcification = int(nodules[a].calcification)
+                    nods[b].sphericity = int(nodules[a].sphericity)
+                    nods[b].margin = int(nodules[a].margin)
+                    nods[b].lobulation = int(nodules[a].lobulation)
+                    nods[b].spiculation = int(nodules[a].spiculation)
+                    nods[b].texture = int(nodules[a].texture)
+                    nods[b].malignancy = int(nodules[a].malignancy)
+    except:
+        print('pid: {}, nodules numbers do not match.')
+        
+        for a in range(len(nodules)):
         nod_mask, nod_bbox, ann_masks = consensus(allNodules[a])
         scan_vol = np.zeros(shape)
         scan_vol[nod_bbox] = nod_mask
@@ -26,7 +48,7 @@ def getAtts(nods, pid):
         final_vol = temp_vol[x1:x2, y1:y2, z1:z2]
         for b in range(len(nods)):
             numIntersect = np.sum(final_vol & nods[b].array)
-            if numIntersect == np.sum(final_vol):
+            if numIntersect == np.sum(final_vol) or numIntersect >= 0.9*np.sum(final_vol):
                 nods[b].subtlety = int(nodules[a].subtlety)
                 nods[b].internalStructure = int(nodules[a].internalStructure)
                 nods[b].calcification = int(nodules[a].calcification)
