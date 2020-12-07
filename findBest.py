b840f56b58b063c5b5321a74169331701acc8e9c
cylee <lcy12544@gmail.com>
2020-12-07 15:33:09 -0500

    update

--- findBest.py
+++ findBest.py
@@ -13,21 +13,40 @@ dicewoFP = float(0)
 
 for predIter in os.listdir(predDir):
     if 'iter_' in predIter:
-        Fconf = open(os.path.join(predDir, predIter, 'eval', 'confusion.txt'), 'r')
-        Fdice = open(os.path.join(predDir, predIter, 'eval', 'dice.txt'), 'r')
-        FwoFP = open(os.path.join(predDir, predIter, 'eval', 'woFP.txt'), 'r')
-        
-        a = Fconf.read()
-        b = re.findall(r'[0-9]+', a)
         try:
+            Fconf = open(os.path.join(predDir, predIter, 'eval', 'confusion.txt'), 'r')
+            a = Fconf.read()
+            b = re.findall(r'[0-9]+', a)
             tempconf = np.array(b, dtype = 'int').reshape((2,2))
         except:
-            print(predIter)
-            print(a)
-            print(b)
-            sdfsdf
-        tempdice = float(Fdice.read())
-        tempwoFP = float(FwoFP.read())
+            Fconf.close()
+            Fconf = open(os.path.join(predDir, predIter, 'eval', 'confusion_1.txt'), 'r')
+            a = Fconf.read()
+            b = re.findall(r'[0-9]+', a)
+            tempconf = np.array(b, dtype = 'int').reshape((2,2))
+            Fconf.close()
+        
+        try:
+            Fdice = open(os.path.join(predDir, predIter, 'eval', 'dice.txt'), 'r')
+            tempdice = float(Fdice.read())
+            assert isinstance(tempdice, float) is True
+        except:
+            Fdice.close()
+            Fdice = open(os.path.join(predDir, predIter, 'eval', 'dice_1.txt'), 'r')
+            tempdice = float(Fdice.read())
+            assert isinstance(tempdice, float) is True
+        
+        try:
+            FwoFP = open(os.path.join(predDir, predIter, 'eval', 'woFP.txt'), 'r')
+            tempwoFP = float(FwoFP.read())
+        except:
+            FwoFP.close()
+            FwoFP = open(os.path.join(predDir, predIter, 'eval', 'woFP_1.txt'), 'r')
+            tempwoFP = float(FwoFP.read())
+            
+        Fconf.close()
+        Fdice.close()
+        FwoFP.close()
         
         if tempconf[0,0] > confusion_matrix[0,0]:
             confusion_matrix = tempconf
