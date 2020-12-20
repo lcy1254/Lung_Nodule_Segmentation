a7075aa3326584ed7e447c59d2e2f2bdb6d6469e
lcy1254 <lcy12544@gmail.com>
2020-12-19 20:37:30 -0500

    update

--- preproc_FPR.py
+++ preproc_FPR.py
@@ -124,3 +124,29 @@ for predIter in os.listdir(predDir):
                                 f.close()
                             
                             total_nod_count += 1
+                        elif numIntersect >= 0.3*np.sum(tempVol):
+                            x,y,z = np.nonzero(tempVol)
+                            x1 = np.min(x)-5
+                            x2 = np.max(x)+5
+                            y1 = np.min(y)-5
+                            y2 = np.max(y)+5
+                            z1 = np.min(z)-5
+                            z2 = np.max(z)+5
+                            nodVol = (img_vol[x1:x2, y1:y2, z1:z2]).astype(np.float32)
+                            
+                            if total_nod_count in testingSet:
+                                split = 'testing'
+                            elif total_nod_count in validationSet:
+                                split = 'validation'
+                            else:
+                                split = 'training'
+                            
+                            path_vol_save = os.path.join(outDir[split], '{}.h5'.format(total_nod_count))
+                            with h5py.File(path_vol_save, 'w') as h5f:
+                                h5f.create_dataset('data', data=nodVol, dtype=np.float32)
+                            path_label_save = os.path.join(outDir[split], '{}.txt'.format(total_nod_count))
+                            with open(path_label_save, 'w+') as f:
+                                f.write('1')
+                                f.close()
+                            
+                            total_nod_count += 1
