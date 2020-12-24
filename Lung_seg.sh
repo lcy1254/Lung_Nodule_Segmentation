a1ee3cde57c674393fe57aa3c8954681296580ea
Chaeyoung Lee <63752452+lcy1254@users.noreply.github.com>
2020-12-24 17:21:29 -0500

    Update Lung_seg.sh

--- Lung_seg.sh
+++ Lung_seg.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-declare -r python='python -B'
+declare -r python='python -B -m pdb'
 
 declare -r script=train_CNNsegmentation3d.py
 
 declare -r path_data=/data/lung_seg
-declare -r path_frozen=$path_data/frozen
-declare -r path_model=$path_data/model
+declare -r path_frozen=$path_data/blaine/frozen
+declare -r path_model=$path_data/blaine/model
 
 #declare -r name=CT_Liver_Lesion_Unet_fuzzy
 #declare -r name=CT_Liver_Lesion_Unet
-declare -r name=CT_Lung_Seg_Unet
+declare -r name=CT_Lung_Seg_Unet4
 
-declare -r path_log=$path_data/logs
-declare -r path_vis=$path_data/visualization
+declare -r path_log=$path_data/blaine/logs
+declare -r path_vis=$path_data/blaine/visualization
 mkdir $path_model
 mkdir $path_log
 mkdir $path_vis
 
 #declare -r cropSize='144 144 144'
 declare -r cropSize='120 120 120'
-declare -r train_gpus='0 1 2' # The GPUs to use for training
-declare -r augment_gpu=3 # The GPU to use for data augmentation. If empty, uses the CPU
-declare -r batch_size=6
+declare -r train_gpus='0 1 2 3' # The GPUs to use for training
+declare -r augment_gpu='' # The GPU to use for data augmentation. If empty, uses the CPU
+declare -r batch_size=8
 #declare -r batch_size=3 # Usually use 6, but getting OOM with fuzzy loss
 #declare -r epoch=250
 declare -r epoch=50 # Previously 500, when validate=true
-declare -r lr=0.0001 # Stage 0
-#declare -r lr=0.00001 # Stage 1
+#declare -r lr=0.0001 # Stage 0
+declare -r lr=0.000008 # Stage 1
 #declare -r lr=0.0000025 # Stage 2
 #declare -r lr=0.000001 # Stage 3
 #declare -r lr=0.0000001 # Stage  4
 declare -r net='uNet' # 'GoogLe' or 'uNet'
-declare -r loadit=0
+declare -r loadit=1
 declare -r window='lung-ct' # Sets preprocessing window. Options: 'liver'
 declare -r batch_norm=1 # Use batch norm if nonzero
 declare -r autotune=0 # Allow Tensorflow to run benchmarks if nonzero
@@ -51,12 +51,12 @@ declare -r display=1 # If nonzero, display training progress
 declare -r masked=0 # If true, set pixels with label -1 to zero
 declare -r validate=0 # If true, run inference on the validation set and save the model with the highest accuracy. Otherwise, saves the model with the lowest training loss
 declare -r criterion='train' # 'train' or 'val', decides which loss will save the model
-declare -r cropping='valid' # 'uniform' (default), 'valid', 'none', etc.
+declare -r cropping='nonzero' # 'uniform' (default), 'valid', 'none', 'nonzero'
 declare -r iou_log_prob=1 # For IOU loss, use squished log probabilities #XXX This is set to CE loss now!
 declare -r freeze_model=1 # If true, freeze the weights and save at each epoch
-declare -r background_loss=0 # If true, under IOU loss, include the background
+declare -r background_loss=1 # If true, under IOU loss, include the background
 
 ##########
 # TRAINING THE MODEL
 ##########
-${python} $script --pTrain $path_data/training --pVal $path_data/validation --pModel $path_model/$name.ckpt --pLog $path_log/training.txt --pVis $path_vis --name $name --trainGPUs $train_gpus --augGPU $augment_gpu --bs $batch_size --ep $epoch --lr $lr --bLo $loadit --net $net --nClass $num_class --cropSize $cropSize --window $window --bBatchNorm $batch_norm --bAutotune $autotune --bBalanced $balanced --minObjectSize $min_object_size --oob_label $oob_label --oob_image_val $oob_image_val --bDisplay $display --bAugmentation $augmentation --maxAugIter $maxAugIter --loss $loss --bMasked $masked --bValidate $validate --criterion $criterion --iou_log_prob $iou_log_prob --bFreeze $freeze_model --pFrozen $path_frozen --bBackgroundLoss $background_loss
+${python} $script --pTrain $path_data/training --pVal $path_data/validation --pModel $path_model/$name.ckpt --pLog $path_log/training.txt --pVis $path_vis --name $name --trainGPUs $train_gpus --augGPU $augment_gpu --bs $batch_size --ep $epoch --lr $lr --bLo $loadit --net $net --nClass $num_class --cropping $cropping --cropSize $cropSize --window $window --bBatchNorm $batch_norm --bAutotune $autotune --bBalanced $balanced --minObjectSize $min_object_size --oob_label $oob_label --oob_image_val $oob_image_val --bDisplay $display --bAugmentation $augmentation --maxAugIter $maxAugIter --loss $loss --bMasked $masked --bValidate $validate --criterion $criterion --iou_log_prob $iou_log_prob --bFreeze $freeze_model --pFrozen $path_frozen --bBackgroundLoss $background_loss
