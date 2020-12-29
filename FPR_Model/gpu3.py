88b8c1dfe3dedaaf0a5744a482938e6f973e93f6
lcy1254 <lcy12544@gmail.com>
2020-12-29 00:14:00 -0500

    update

--- FPR_Model/gpu3.py
+++ FPR_Model/gpu3.py
@@ -16,67 +16,70 @@ from tensorflow.keras import backend as K
 import metricsHistory as mh
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
+tf.config.experimental.set_memory_growth(gpus[0,1,2,3], True)
 
-mode_run = 'train' #train or test
+strategy = tf.distribute.MirroredStrategy()
+with strategy.scope():
+#tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
 
-##----------------------------- Parameters -----------------------------------##
-n_classes = 2
-sideLength = 50
-batch_size = 32
-max_epochs = 50
-period_checkpoint = 1
-current_file_name = os.path.basename(__file__)[:-3]
+    mode_run = 'train' #train or test
 
-##------------------------------ Dataset -------------------------------------##
-#Load list of IDs
-trainingdataDir = '/data/lung_seg/FPR/nodule_files/training'
-validationdataDir = '/data/lung_seg/FPR/nodule_files/validation'
+    ##----------------------------- Parameters -----------------------------------##
+    n_classes = 2
+    sideLength = 50
+    batch_size = 16
+    max_epochs = 50
+    period_checkpoint = 1
+    current_file_name = os.path.basename(__file__)[:-3]
 
-traininglistIDs = [re.findall(r'[0-9]+', file)[0] for file in os.listdir(trainingdataDir) if '.h5' in file]
-validationlistIDs = [re.findall(r'[0-9]+', file)[0] for file in os.listdir(validationdataDir) if '.h5' in file]
+    ##------------------------------ Dataset -------------------------------------##
+    #Load list of IDs
+    trainingdataDir = '/data/lung_seg/FPR/nodule_files/training'
+    validationdataDir = '/data/lung_seg/FPR/nodule_files/validation'
 
-training_generator = volume.DataGenerator(traininglistIDs, trainingdataDir, batch_size, v_size=sideLength, n_channels=1, n_classes=n_classes, shuffle=True)
+    traininglistIDs = [re.findall(r'[0-9]+', file)[0] for file in os.listdir(trainingdataDir) if '.h5' in file]
+    validationlistIDs = [re.findall(r'[0-9]+', file)[0] for file in os.listdir(validationdataDir) if '.h5' in file]
 
-validation_generator = volume.DataGenerator(validationlistIDs, validationdataDir, batch_size, v_size=sideLength, n_channels=1, n_classes=n_classes, shuffle=True)
+    training_generator = volume.DataGenerator(traininglistIDs, trainingdataDir, batch_size, v_size=sideLength, n_channels=1, n_classes=n_classes, shuffle=True)
 
-##------------------------------ Model ---------------------------------------##
-#Create
-model = models.VGG16(sideLength)
-#model = models.alexNet(sideLength)
-#model = models.resNet(sideLength)
-model.summary()
+    validation_generator = volume.DataGenerator(validationlistIDs, validationdataDir, batch_size, v_size=sideLength, n_channels=1, n_classes=n_classes, shuffle=True)
 
-#Track accuracy and loss in real-time
-#if jupyter notebook:
-log_dir = "/data/lung_seg/FPR/VGG16/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-file_writer = tf.summary.create_file_writer(log_dir + "/metrics")
-file_writer.set_as_default()
+    ##------------------------------ Model ---------------------------------------##
+    #Create
+    model = models.VGG16(sideLength)
+    #model = models.alexNet(sideLength)
+    #model = models.resNet(sideLength)
+    model.summary()
 
-#python script
-saving_path = log_dir
-history = mh.MetricsHistory(saving_path=os.path.join(saving_path,'metricsHistory', current_file_name+'.csv'))
+    #Track accuracy and loss in real-time
+    #if jupyter notebook:
+    log_dir = "/data/lung_seg/FPR/VGG16/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    file_writer = tf.summary.create_file_writer(log_dir + "/metrics")
+    file_writer.set_as_default()
 
-#Checkpoints
-checkpoints = ModelCheckpoint(log_dir + '/checkpoints/' + current_file_name + '_{epoch:02d}' + '.hd5f', save_weights_only=True, period=period_checkpoint)
+    #python script
+    saving_path = log_dir
+    history = mh.MetricsHistory(saving_path=os.path.join(saving_path,'metricsHistory', current_file_name+'.csv'))
 
-if mode_run == 'train':
-    #Compile
-    def scheduler(epoch, lr):
-        learning_rate = 0.1
-        if epoch > 10:
-            learning_rate = 0.02
-        if epoch > 15:
-            learning_rate = 0.01
-        if epoch > 30:
-            learning_rate = 0.005
-        tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
-        return learning_rate
+    #Checkpoints
+    checkpoints = ModelCheckpoint(log_dir + '/checkpoints/' + current_file_name + '_{epoch:02d}' + '.hd5f', save_weights_only=True, period=period_checkpoint)
+
+    if mode_run == 'train':
+        #Compile
+        def scheduler(epoch, lr):
+            learning_rate = 0.1
+            if epoch > 10:
+                learning_rate = 0.02
+            if epoch > 15:
+                learning_rate = 0.01
+            if epoch > 30:
+                learning_rate = 0.005
+            tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
+            return learning_rate
+            
+        lr_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
+        tensorboard = TensorBoard(log_dir = log_dir, histogram_freq = 1)
         
-    lr_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
-    tensorboard = TensorBoard(log_dir = log_dir, histogram_freq = 1)
-    
-    model.compile(optimizer=tf.keras.optimizers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])
-    
-    model.fit_generator(generator=training_generator, epochs=max_epochs, verbose=1, validation_data=validation_generator, callbacks=[history, checkpoints, lr_callback, tensorboard])
+        model.compile(optimizer=tf.keras.optimizers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+        
+        model.fit_generator(generator=training_generator, epochs=max_epochs, verbose=1, validation_data=validation_generator, callbacks=[history, checkpoints, lr_callback, tensorboard])
