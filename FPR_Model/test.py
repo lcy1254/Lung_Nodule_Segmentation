9e60b8a2038c20b3601ded12b5bfeb8983251593
lcy1254 <lcy12544@gmail.com>
2020-12-30 01:10:13 -0500

    up

--- FPR_Model/test.py
+++ FPR_Model/test.py
@@ -4,6 +4,7 @@ import models
 from volume import testDataGenerator
 import re
 import os
+import math
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
@@ -13,7 +14,11 @@ tf.config.experimental.set_memory_growth(gpus[3], True)
 strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
     testDir = '/data/lung_seg/FPR/nodule_files/testing'
-    testinglistIDs = [re.findall(r'[0-9]+', file)[0] for file in os.listdir(testDir) if '.h5' in file]
+    testinglistIDs = [int(re.findall(r'[0-9]+', file)[0]) for file in os.listdir(testDir) if '.h5' in file]
+    
+    a = len(testinglistIDs)
+    print('total of {} testing images'.format(a))
+    step = math.ceil(a/16)
     
     batch_size = 16
     sideLength = 48
@@ -23,7 +28,7 @@ with strategy.scope():
 
     # Load best weights.
     model.load_weights("/data/lung_seg/FPR/VGG16/logs/fit/20201229-021400/checkpoints/gpu3_40.hd5f")
-    prediction = model.predict(test_generator, verbose=1)
+    prediction = model.predict(test_generator, verbose=1, steps=step)
     
     savePath = '/data/lung_seg/FPR/VGG16/logs/fit/20201229-021400'
     with open(os.path.join(savePath, 'prediction.txt'), 'w+') as f:
