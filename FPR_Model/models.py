64cebe666849776d2e5450dc3af1f1a141880742
lcy1254 <lcy12544@gmail.com>
2020-12-30 03:32:11 -0500

    up

--- FPR_Model/models.py
+++ FPR_Model/models.py
@@ -85,10 +85,10 @@ def alexNet(sideLength):
     '''AlexNet 3D CNN Implementation'''
     input = keras.Input(shape=(sideLength, sideLength, sideLength, 1))
     
-    x = layers.Conv3D(filters=96, kernel_size=11, strides=4, padding='same')(input)
+    x = layers.Conv3D(filters=96, kernel_size=11, strides=3, padding='same')(input)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
-    x = layers.MaxPooling3D((3,3,3), strides=2)(x)
+    x = layers.MaxPooling3D(2, strides=1)(x)
     
     x = layers.Conv3D(256, 5, 1, padding='same')(x)
     x = layers.BatchNormalization()(x)
@@ -106,7 +106,7 @@ def alexNet(sideLength):
     x = layers.Conv3D(256, 3, 1, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
-    x = layers.MaxPooling3D((3,3,3), (2,2,2))(x)
+    x = layers.MaxPooling3D(2, 1)(x)
     
     x = layers.Flatten()(x)
     x = layers.Dense(4096, activation='relu')(x)
