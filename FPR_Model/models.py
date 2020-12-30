8de74ac16546dced072fca4204d7517dda875171
lcy1254 <lcy12544@gmail.com>
2020-12-30 03:29:33 -0500

    up

--- FPR_Model/models.py
+++ FPR_Model/models.py
@@ -85,25 +85,25 @@ def alexNet(sideLength):
     '''AlexNet 3D CNN Implementation'''
     input = keras.Input(shape=(sideLength, sideLength, sideLength, 1))
     
-    x = layers.Conv3D(filters=96, kernel_size=11, strides=1)(input)
+    x = layers.Conv3D(filters=96, kernel_size=11, strides=4, padding='same')(input)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
-    x = layers.MaxPooling3D((3,3,3))(x)   #strides=(2,2,2)
+    x = layers.MaxPooling3D((3,3,3), strides=2)(x)
     
-    x = layers.Conv3D(256, 5, 1)(x)
+    x = layers.Conv3D(256, 5, 1, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
-    x = layers.MaxPooling3D((3,3,3), strides=(2,2,2))(x)
+    x = layers.MaxPooling3D(2, strides=1)(x)
     
-    x = layers.Conv3D(384, 3, 1)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
-    
-    x = layers.Conv3D(384, 3, 1)(x)
+    x = layers.Conv3D(384, 3, 1, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
     
-    x = layers.Conv3D(256, 3, 1)(x)
+    x = layers.Conv3D(384, 3, 1, padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    
+    x = layers.Conv3D(256, 3, 1, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
     x = layers.MaxPooling3D((3,3,3), (2,2,2))(x)
