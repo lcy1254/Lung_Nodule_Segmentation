86307186cfd4d2c4efea312a7fc0dad5f22fd4e3
lcy1254 <lcy12544@gmail.com>
2021-01-02 01:39:32 -0500

    up

--- FPR_Model/test.py
+++ FPR_Model/test.py
@@ -7,33 +7,34 @@ import os
 import math
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
-tf.config.experimental.set_memory_growth(gpus[1], True)
+tf.config.experimental.set_visible_devices(gpus[2], 'GPU')
 tf.config.experimental.set_memory_growth(gpus[2], True)
-tf.config.experimental.set_memory_growth(gpus[3], True)
+'''
 strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
-    testDir = '/data/lung_seg/FPR/nodule_files/testing'
-    testinglistIDs = [int(re.findall(r'[0-9]+', file)[0]) for file in os.listdir(testDir) if '.h5' in file]
+'''
+testDir = '/data/lung_seg/FPR/nodule_files/testing'
+testinglistIDs = [int(re.findall(r'[0-9]+', file)[0]) for file in os.listdir(testDir) if '.h5' in file]
+
+#epochs = [i for i in range(1, 51)]
+
+epochs = [16]
+a = len(testinglistIDs)
+print('total of {} testing images'.format(a))
+step = math.ceil(a/16)
+
+batch_size = 128
+sideLength = 48
+
+test_generator = testDataGenerator(testinglistIDs, testDir, batch_size=batch_size, v_size=sideLength)
+model = models.VGG16(sideLength)
+
+for epoch in epochs:
+    model.load_weights("/data/lung_seg/FPR/no_aug/2021-01-01_18:44:52/checkpoints/no_aug_{}.hd5f".format(str(epoch).zfill(2)))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    prediction = model.evaluate(test_generator, verbose=1)
     
-    epochs = [i for i in range(1, 51)]
-    
-    a = len(testinglistIDs)
-    print('total of {} testing images'.format(a))
-    step = math.ceil(a/16)
-    
-    batch_size = 256
-    sideLength = 48
-    
-    test_generator = testDataGenerator(testinglistIDs, testDir, batch_size=batch_size, v_size=sideLength)
-    model = models.VGG16(sideLength)
-    
-    for epoch in epochs:
-        model.load_weights("/data/lung_seg/FPR/VGG16/second/2020/12/31-15:56:09/checkpoints/gpu3_{}.hd5f".format(str(epoch).zfill(2)))
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        prediction = model.evaluate(test_generator, verbose=1)
-        
-        savePath = '/data/lung_seg/FPR/VGG16/second/2020/12/31-15:56:09/evaluation'
-        if not os.path.isdir(savePath): os.mkdir(savePath)
-        with open(os.path.join(savePath, 'prediction_epoch{}.txt'.format(epoch)), 'w+') as f:
-            f.write(str(prediction))
+    savePath = '/data/lung_seg/FPR/no_aug/2021-01-01_18:44:52/evaluation'
+    if not os.path.isdir(savePath): os.mkdir(savePath)
+    with open(os.path.join(savePath, 'prediction_epoch{}.txt'.format(epoch)), 'w+') as f:
+        f.write(str(prediction))
