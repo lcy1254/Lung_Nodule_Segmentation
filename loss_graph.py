3148f22d21a773c24ec8766a2a232e2f9235cc5a
lcy1254 <lcy12544@gmail.com>
2021-01-16 23:36:01 -0500

    update loss_graph

--- loss_graph.py
+++ loss_graph.py
@@ -1,12 +1,14 @@
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+import os
 
 #Usage: loss_graph.py [full path to training.txt] [where to start graph (iter)]
 #example: loss_graph.py /data/lung_seg/logs/training.txt 98
 #if graph should start from first iteration, leave blank 
 
 fileDir = sys.argv[1]
+outDir = '/home/lcy'
 if len(sys.argv) == 3:
 	iter = int(sys.argv[2])
 else:
@@ -23,9 +25,13 @@ if iter == 0:
 else:
 	loss = np.array(loss).astype(np.float)[iter:]
 
+with open(os.path.join(outDir, 'loss.txt'), 'w+') as f:
+    for num in loss:
+        f.write('%.5f\n' % num)
+'''
 x = np.arange(loss.size)
 
 plt.plot(x, loss, 'o')
 plt.title('Loss')
 plt.show(block=True)
-
+'''
