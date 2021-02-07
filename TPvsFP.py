5e9a30d184cc105145a28570ec09e06cf467724b
cylee <lcy12544@gmail.com>
2021-02-07 18:02:50 -0500

    create script TPvsFP for seeing relationship between sensitivity and false positive rate

---
+++ TPvsFP.py
@@ -0,0 +1,62 @@
+import os
+import re
+import numpy as np
+import csv 
+
+predDir = '/data/lung_seg/inference'
+
+TPR = []
+FPR = []
+
+for predIter in os.listdir(predDir):
+    if 'iter_' in predIter:
+        try:
+            Fconf = open(os.path.join(predDir, predIter, 'eval', 'confusion.txt'), 'r')
+            a = Fconf.read()
+            b = re.findall(r'[0-9]+', a)
+            tempconf = np.array(b, dtype = 'int').reshape((2,2))
+            tp=tempconf[0,0]
+            fp=tempconf[0,1]
+            TPR.append(tp)
+            FPR.append(fp)
+        except ValueError:
+            Fconf.close()
+            Fconf = open(os.path.join(predDir, predIter, 'eval', 'confusion.txt'), 'r')
+            a = Fconf.readline().strip()
+            a2 = Fconf.readline().strip()
+            a = a.strip('[] ')
+            a2 = a2.strip('[] ')
+            a = a.split()
+            a2 = a2.split()
+            a = [int(float(val)) for val in a]
+            a2 = [int(float(val)) for val in a2]
+            tempconf = np.zeros((2,2))
+            tp=a[0]
+            fp=a[1]
+            TPR.append(tp)
+            FPR.append(fp)
+        except:
+            Fconf.close()
+            Fconf = open(os.path.join(predDir, predIter, 'eval', 'confusion_1.txt'), 'r')
+            a = Fconf.read()
+            b = re.findall(r'[0-9]+', a)
+            tempconf = np.array(b, dtype = 'int').reshape((2,2))
+            tp=tempconf[0,0]
+            fp=tempconf[0,1]
+            TPR.append(tp)
+            FPR.append(fp)
+            
+        Fconf.close()
+
+print('TPR: ', str(TPR))
+print('FPR: ', str(FPR))
+
+with open('/data/lung_seg/TPvsFP.csv', mode='w+') as f:
+    f_writer = csv.writer(f)
+    f_writer.writerow(TPR)
+    f_writer.writerow(FPR)
+
+file = open('/data/lung_seg/TPvsFP.txt', 'w+')
+file.write(str(TPR))
+file.write(str(FPR))
+file.close()
