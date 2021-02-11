7ce99d0498801c24017e019b6539407e0a4332cc
cylee <lcy12544@gmail.com>
2021-02-10 22:37:51 -0500

    up

--- resize_mask.py
+++ resize_mask.py
@@ -13,19 +13,20 @@ gtDir = '/data/improved_res_lung/improved_res_nifti_files'
 for predIter in os.listdir(predDir):
     print(predIter)
     alr = os.listdir(os.path.join(predDir, predIter, 'prediction'))
-    for predFile in os.listdir(os.path.join(predDir, predIter, 'prediction')):
-        if '.nii.gz' in predFile and predFile not in alr:
-            try:
-                img_num = int(re.findall(r'[0-9]+', predFile)[0])
-                print(img_num)
-                label_file = nib.load(os.path.join(predDir, predIter, 'prediction', predFile))
-                vol = np.array(label_file.dataobj).astype(np.int32)
-                gt = nib.load(os.path.join(gtDir, 'labels-{}.nii.gz'.format(img_num)))
-                new_shape = np.array(gt.dataobj.shape[:3])
-                old_shape = np.array(vol.shape[:3])
-                resize_factor = new_shape/old_shape
-                resized_image = scipy.ndimage.zoom(vol, resize_factor)
-                img = nib.Nifti1Image(resized_image, None)
-                nib.save(img, os.path.join(outDir,predIter,'prediction',predFile))
-            except:
-                continue 
+    if 'iter_' in predIter:
+        for predFile in os.listdir(os.path.join(predDir, predIter, 'prediction')):
+            if '.nii.gz' in predFile and predFile not in alr:
+                try:
+                    img_num = int(re.findall(r'[0-9]+', predFile)[0])
+                    print(img_num)
+                    label_file = nib.load(os.path.join(predDir, predIter, 'prediction', predFile))
+                    vol = np.array(label_file.dataobj).astype(np.int32)
+                    gt = nib.load(os.path.join(gtDir, 'labels-{}.nii.gz'.format(img_num)))
+                    new_shape = np.array(gt.dataobj.shape[:3])
+                    old_shape = np.array(vol.shape[:3])
+                    resize_factor = new_shape/old_shape
+                    resized_image = scipy.ndimage.zoom(vol, resize_factor)
+                    img = nib.Nifti1Image(resized_image, None)
+                    nib.save(img, os.path.join(outDir,predIter,'prediction',predFile))
+                except:
+                    continue
