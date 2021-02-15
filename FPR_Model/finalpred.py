2946371fd0cb7eb04952ca430c09f4f54782359a
cylee <lcy12544@gmail.com>
2021-02-15 03:08:58 -0500

    update final_pred for VGG aug

--- FPR_Model/finalpred.py
+++ FPR_Model/finalpred.py
@@ -27,7 +27,7 @@ testinglistIDs = [int(re.findall(r'[0-9]+', file)[0]) for file in os.listdir(tes
 print('whole testing list IDs: ' + str(testinglistIDs))
 
 #epochs = [i for i in range(1, 51)]
-epochs = [34]
+epochs = [33]
 #CHANGE EPOCHS FOR 200 EPOCH RUNS
 
 a = len(testinglistIDs)
@@ -37,14 +37,25 @@ batch_size = 2
 sideLength = 48
 
 test_generator = testDataGenerator(testinglistIDs, testDir, batch_size=batch_size, v_size=sideLength)
-model = models.alexNet(sideLength)
+model = models.VGG16(sideLength)
 
 for epoch in epochs:
-    model.load_weights("/data/lung_seg/FPR/alexNet/aug/2021-01-02_01:55:28/checkpoints/alex_aug_{}.hd5f".format(str(epoch).zfill(2)))
+    model.load_weights("/data/lung_seg/FPR/VGG16/aug2/2021-01-04_03:41:48/checkpoints/vgg_aug_{}.hd5f".format(str(epoch).zfill(2)))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     prediction = model.predict(test_generator, verbose=1)
     
-    savePath = '/media/data_crypt_2/alexNet/eval-final'
+    savePath = '/media/data_crypt_2/VGG/VGG_AUG_eval-final'
     if not os.path.isdir(savePath): os.mkdir(savePath)
-    f = csv.writer(open(os.path.join(savePath, 'evaluation_epoch{}.csv'.format(epoch)), 'w+'))
+    f = csv.writer(open(os.path.join(savePath, 'predictions_epoch{}.csv'.format(epoch)), 'w+'))
     f.writerow(prediction)
+
+y_truth = []
+for i in testinglistIDs:
+    path = os.path.join(testDir, '{}.txt'.format(i))
+    with open(path, 'r') as f:
+        y = int(f.readline().strip())
+        y_truth.append(y)
+true_save_path = '/media/data_crypt_2/VGG/y_truth'
+if not os.path.isdir(true_save_path): os.mkdir(true_save_path)
+f = csv.writer(open(os.path.join(true_save_path, 'y_truth.csv'), 'w+'))
+f.writerow(y_truth)
